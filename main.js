0c13a17012ad858277cec45284a1c2fd000989dc
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-03-02 03:00:59 +0000

    got the api pulling info that I want

--- main.js
+++ main.js
@@ -2,19 +2,19 @@
 let weatherObject = [];
 let lat=85.04
 let lon=84.50
-let temperature
 let tempF
-let tempC
 let tempK
+let tempC = '0';
 let userZip
-let weatherLink = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,daily,alerts&appid=b18094427e022579a39c568a994093a0';
+let appID = 'b18094427e022579a39c568a994093a0'
+let weatherLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=${appID}`;
 //set the temperature variable to fill in the temerature box
 let temperature = document.querySelector(".temperature")
 //make this the thing that isn't innerHTML
 temperature.innerHTML = tempF
 //need to set up the temperature-celsius class on the html side
-let tempC = document.querySelector(".temperatureCelsius")
-temperatureCelsius.innerHTML = tempC
+// tempC = document.querySelector(".temperatureCelsius")
+// tempC.innerHTML = tempC
 
 
 
@@ -23,7 +23,7 @@ async function getWeather() {
     //explains what the function is waiting for
       let response = await axios.get(weatherLink)
       console.log(response);
-      weatherObject = response;
+      weatherObject.data = response;
 }
 
 getWeather();
