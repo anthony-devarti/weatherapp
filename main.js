8a94459e238b6f860a09cc3a69612630f00d6055
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-03-02 21:22:59 +0000

    got all of the fields working as expected

--- main.js
+++ main.js
@@ -1,29 +1,16 @@
-//define some variables that I need later
-// let weatherObject = [{
-//   "city":"",
-//   "tempK":"",
-//   "condition": ""
-
-// }];
 let city
 let condition
-//lat and lon probably won't be needed
-// let lat=85.04 //this is a placeholder and should be replaced with user input
-// let lon=84.50 //this is also a placeholder
+let description
 let tempK //this will be set by the result
 let tempC = tempK-273.15; //this will be set by a function after the result
 let tempF = tempC*(9/5)+32;//this will be set by a function after the result
 let userZip = 40502; //change to 0 to make it an empty value
 let appID = 'b18094427e022579a39c568a994093a0'
-//this should allow the lat, lon, and app ID to be added in via variables, rather than typed into the url
+//lat and lon probably won't be needed
+// let lat=85.04 //this is a placeholder and should be replaced with user input
+// let lon=84.50 //this is also a placeholder
 let weatherLink = `https://api.openweathermap.org/data/2.5/weather?zip=${userZip},us&appid=${appID}`;
-//set the temperature variable to fill in the temerature box
-let temperature = document.querySelector(".temperature")
-//make this the thing that isn't innerHTML
-//temperature.innerHTML = tempF
-//need to set up the temperature-celsius class on the html side
-// tempC = document.querySelector(".temperatureCelsius")
-// tempC.innerHTML = tempC
+
 
 
 
@@ -35,24 +22,24 @@ async function getWeather() {
       console.log(response);
       weatherObject = await response.json;
       city = response.data.name
-      console.log(city)
       tempK = response.data.main.temp;
-      console.log(tempK);
-      //Need to pull the condition next
-      // condition = response.data.main.weather.description; //work on making this save something valid to the condition variable
-      // console.log(condition);
-      //lets push these values into their right positions on the webpage
+      description = response.data.weather[0].description;
+      condition = response.data.weather[0].main;
       let userCity = document.getElementById("city")
       userCity.textContent =  city;
       let tempKelvin = document.getElementById("kelvin")
-      tempKelvin.textContent = tempK + ' K'
+      tempKelvin.textContent = Math.floor(tempK) + ' K'
       let tempFaren = document.getElementById("farenheit")
       tempFaren.textContent = Math.floor((tempK-273.15)*(9/5)+32)+ ' F'
       let tempCels = document.getElementById('celsius')
       tempCels.textContent = Math.floor(tempK-273.15) + ' C';
-      //push in the description
-      let newDescription = document.getElementById("condition")
-      newDescription.textContent = 'This is where the description gets pushed'
+      let newCondition= document.getElementById("condition");
+      newCondition.textContent = condition;
+      let otherInfo = document.getElementById("other");
+      otherInfo.innerText = description
+      console.log(city)
+      console.log(description);
+      console.log(condition);
   } catch (error) {
     console.error('error')
   }
@@ -62,12 +49,20 @@ async function getWeather() {
 
 getWeather();  //this is not running before the declarations below it.  Maybe because they aren't functions 
 
-//push in the temperatures
-
-//other info goes here
-let otherInfo = document.getElementById("other");
-otherInfo.innerText = 'text about the weather'
 
 
-//addData();
-//update();
+function pushText() {
+  let userCity = document.getElementById("city")
+  userCity.textContent =  city;
+  let tempKelvin = document.getElementById("kelvin")
+  tempKelvin.textContent = tempK + ' K'
+  let tempFaren = document.getElementById("farenheit")
+  tempFaren.textContent = Math.floor((tempK-273.15)*(9/5)+32)+ ' F'
+  let tempCels = document.getElementById('celsius')
+  tempCels.textContent = Math.floor(tempK-273.15) + ' C';
+  //push in the description
+  let newDescription = document.getElementById("description")
+  newDescription.textContent = description;
+  let otherInfo = document.getElementById("other");
+  otherInfo.innerText = 'text about the weather'
+}
