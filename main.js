3bf018214bada10b4e4c6622750a048fa0346f3c
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-03-04 12:59:39 +0000

    cleaning up the dev branch since it looks more and more like this is what I'm going to be demoing

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ async function getWeather() {
   try{
     //explains what the function is waiting for
       let response = await axios.get(weatherLink)
-      //console.log(response);
+      console.log(response);
       weatherObject = await response.json;
       city = response.data.name
       tempK = response.data.main.temp;
@@ -83,20 +83,6 @@ function whoopsie(){
   document.getElementById("error").style.display="block";
 }
 
-//make a function to change the background color based on the weather icon
-// function bgUpdate(){
-// if (weatherIcon==='10d'||'02d') {
-//   //change css to yellow/sunny
-//   console.log("i'm trying to change the color")
-//   document.body.style.backgroundImage="linear-gradient(rgb(212, 131, 10), rgb(140, 200, 240))";
-// } else if (weatherIcon==='03d'||'04d'||'50d') {
-//   //change css to grey/overcast
-//   document.body.style.backgroundImage="linear-gradient(rgb(46,46,46), rgb(140, 200, 240))"
-// } else if (weatherIcon==='09d'||'10d'||'11d'||'13d') {
-//   //change css to blue/precipitation
-// }
-// }
-
 function bgUpdate(){
   switch (weatherIcon) {
     case '01d': document.getElementById('background').style.backgroundImage="linear-gradient(rgb(245, 202, 61), white)"
@@ -105,7 +91,7 @@ function bgUpdate(){
       break;
     case '03d': document.getElementById('background').style.backgroundImage='linear-gradient(rgb(65, 93, 95), white)'
       break;
-    case '04d': document.getElementById('background').style.backgroundImage='linear-gradient(rgb(143, 188, 209), white)';
+    case '04d': document.getElementById('background').style.backgroundImage='linear-gradient(rgb(143, 188, 209), white)'
       break;
     case '09d': document.getElementById('background').style.backgroundImage='linear-gradient(rgb(99, 107, 226), white)'
       break;
@@ -118,6 +104,7 @@ function bgUpdate(){
     case '50d': document.getElementById('background').style.backgroundImage='linear-gradient(rgb(70, 69, 68), white)'
       break;
     case 'none': document.getElementById('background').style.backgroundImage="linear-gradient(rgb(138, 76, 117), rgb(255, 255, 255))"
+    //this wasn't working because it was night time, and the night icons were showing. so the bg wasn't updating right.  If this is somewthing I want to use at night, I'll refactor to include these
   };
   console.log('this is running')
 }
